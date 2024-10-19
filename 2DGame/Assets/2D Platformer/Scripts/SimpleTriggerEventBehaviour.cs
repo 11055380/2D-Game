b468d903cb29d0b9052545f794f39ab5972d4fcc
Gail <11055380@uvu.edu>
2024-10-19 13:04:45 -0600

    Debug attempt

--- 2DGame/Assets/2D Platformer/Scripts/SimpleTriggerEventBehaviour.cs
+++ 2DGame/Assets/2D Platformer/Scripts/SimpleTriggerEventBehaviour.cs
@@ -7,10 +7,25 @@ public class SimpleTriggerEventBehaviour : MonoBehaviour
 {
     public UnityEvent triggerEvent;
 
+    public Animator animator;
+
     private void OnTriggerEnter(Collider other)
     {
         // Trigger the event and test with a debug message
         triggerEvent.Invoke();
         Debug.Log("Player interacted with the object!");
+
+        // Play an animation
+        //animator.Play("AnimationName");
+
+        // Make the object disappear
+        other.gameObject.SetActive(false);
+
+        // Make the object fall
+        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        if (otherRigidbody != null)
+        {
+            otherRigidbody.AddForce(Vector3.down * 100f, ForceMode.Impulse);
+        }
     }
 }
