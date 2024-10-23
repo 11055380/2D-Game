8270ab0695359b3f502f3f3c09aa798786e0663b
Gail <11055380@uvu.edu>
2024-10-23 17:04:25 -0600

    Health bar to enemy1

--- 2DGame/Assets/2D Platformer/Scripts/SimpleTriggerEventBehaviour.cs
+++ 2DGame/Assets/2D Platformer/Scripts/SimpleTriggerEventBehaviour.cs
@@ -1,19 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Threading;
 
 public class EnemyFade : MonoBehaviour
 {
     public float fadeSpeed = 2f;
     public float detectionRadius = 1f; // Adjust detection radius as needed
 
+    //Health
+    public HealthBar healthBar;
+    public int maxHealth = 100;
+    public int currentHealth;
+    
+    //Health
+    void Start()
+    {
+        currentHealth = 100;
+        GameObject healthBarObject = GameObject.Find("Health Bar2");
+        if (healthBarObject != null)
+        {
+            healthBar = healthBarObject.GetComponent<HealthBar>();
+            healthBar.SetMaxHealth(maxHealth);
+            healthBar.SetHealth(currentHealth);
+        }
+        else
+        {
+            Debug.LogError("Health Bar2 not found by name.");
+        }
+    }
     private void Update()
     {
         if (Physics2D.OverlapCircle(transform.position, detectionRadius, LayerMask.GetMask("Player")))
         {
             StartCoroutine(FadeOut());
         }
+    }
+    
+    //Health
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 
 
@@ -23,6 +53,13 @@ public class EnemyFade : MonoBehaviour
         Color originalColor = renderer.color;
 
         float elapsedTime = 0f;
+        //Health
+        for (int i=0; i<=10; i++)
+        {
+            TakeDamage(10); 
+            Thread.Sleep(500);
+        }
+        
         while (elapsedTime < 1f)
         {
             elapsedTime += Time.deltaTime / fadeSpeed;
