3509deb4971a3a1e9823a7327e6b747a4614c77f
Gail <11055380@uvu.edu>
2024-11-07 14:28:01 -0700

    Adjusted script for coroutine

--- 2DGame/Assets/2D Platformer/Scripts/TriggerParticleEffect.cs
+++ 2DGame/Assets/2D Platformer/Scripts/TriggerParticleEffect.cs
@@ -6,13 +6,30 @@ using UnityEngine;
 public class TriggerParticleEffect : MonoBehaviour
 {
     public int particleAmount = 10;
+    public int firstEmissionAmount = 5;
+    public int secondEmissionAmount = 10;
+    public int thirdEmissionAmount = 30;
+    public float delayBetweenEmissions = .2f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<Rigidbody2D>())  // Check for Rigidbody2D instead of CharacterController
         {
-            GetComponent<ParticleSystem>().Emit(particleAmount);
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        // First emission
+        GetComponent<ParticleSystem>().Emit(firstEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+
+        // Second emission
+        GetComponent<ParticleSystem>().Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+
+        // Third emission
+        GetComponent<ParticleSystem>().Emit(thirdEmissionAmount);
     }
 
 
